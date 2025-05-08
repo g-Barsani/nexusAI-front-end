fb1578c12ca69b3dd68d69aa9e251aa5e94e4ebc
unknown <gabrielbarsani@gmail.com>
2025-05-08 17:36:26 -0300

    Create header and body components

--- nexus-ai/src/app/app.component.ts
+++ nexus-ai/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
