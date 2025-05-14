2b4ef15da1a09962d00015ddafbc6080d66ec779
unknown <gabrielbarsani@gmail.com>
2025-05-14 13:05:54 -0300

    Corrigir problema de rotas na /home

--- nexus-ai/src/app/app.routes.ts
+++ nexus-ai/src/app/app.routes.ts
@@ -1,14 +1,20 @@
 
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
+
+
+
 import { BodyComponent } from './body/body.component';
+import { MainComponent } from './main/main.component';
+
+
 
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: HeaderComponent},
-    { path: 'home', component: BodyComponent},
+    { path: 'home', component: MainComponent},
+
 
     { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect empty path to login
 ];
